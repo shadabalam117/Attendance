2a9d6d09d76df005593765b0281cfaab4192a9a3
Shadab Alam <easytutorials210@gmail.com>
2025-08-03 20:57:19 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0SMRG39Ojbu4hHl33YeY90Zpz7dSCleM",
+  authDomain: "attendance-42c59.firebaseapp.com",
+  projectId: "attendance-42c59",
+  storageBucket: "attendance-42c59.firebasestorage.app",
+  messagingSenderId: "422006192147",
+  appId: "1:422006192147:web:8cfa63852cded30c9567e6",
+  measurementId: "G-EBE7KW8M4C"
+};
